5d264b005ee402a8fe80447aadb4f2bee6cb8bbe
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-07-19 11:55:59 +0200

    organize server

--- server/app.py
+++ server/app.py
@@ -1,12 +1,10 @@
 from flask import Flask
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+load_dotenv()  # take environment variables from .env.
 
 if __name__ == '__main__':
     app.run()
