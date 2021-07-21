682323b6006ab4508b9a5bfe05a06459e44162b7
Dylan Cornelie <dcorneli@etu.utc.fr>
2021-07-21 11:08:31 +0200

    Finitions UI fenetre de chat correction style (en cours)

--- client/src/components/chat/AddParticipantPopUp.js
+++ client/src/components/chat/AddParticipantPopUp.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { showAddParticipant } from '../../actions/chat.action';
-import QRCode  from 'qrcode.react';
+import QRCode from 'qrcode.react';
 
 const AddParticipantPopUp = () => {
   const dispatch = useDispatch();
   const [showQrCode, setShowQrCode] = useState(false);
-  const viewWidth = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  const viewHeight = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  const [addByLogin, setAddByLogin] = useState(false);
 
+  const viewWidth = Math.max(
+    document.documentElement.clientWidth || 0,
+    window.innerWidth || 0
+  );
+  const viewHeight = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  );
 
   return (
     <div
@@ -16,7 +23,15 @@ const AddParticipantPopUp = () => {
       onClick={() => dispatch(showAddParticipant())}
     >
       {showQrCode ? (
-        <QRCode className='qrcode' value='heek yeaah' size= {(viewWidth*0.8 >viewHeight*0.7) ? viewHeight*0.7 : viewWidth*0.8}/>
+        <QRCode
+          className='qrcode'
+          value='heek yeaah'
+          size={
+            viewWidth * 0.8 > viewHeight * 0.7
+              ? viewHeight * 0.7
+              : viewWidth * 0.8
+          }
+        />
       ) : (
         <div
           className='context-menu-button-container'
@@ -24,8 +39,32 @@ const AddParticipantPopUp = () => {
             e.stopPropagation();
           }}
         >
-          <button className='context-menu-button' onClick={()=>setShowQrCode(!showQrCode)}>Show QR code</button>
-          <button className='context-menu-button'>Add by login</button>
+          {addByLogin ? (
+            <div className='addbylogin-modal'>
+              <input type='text' placeholder='login' />
+              <div className='addbylogin-button-container'>
+                <button onClick={() => setAddByLogin(!addByLogin)}>
+                  Cancel
+                </button>
+                <button>Add</button>
+              </div>
+            </div>
+          ) : (
+            <>
+              <button
+                className='context-menu-button'
+                onClick={() => setShowQrCode(!showQrCode)}
+              >
+                Show QR code
+              </button>
+              <button
+                className='context-menu-button'
+                onClick={() => setAddByLogin(!addByLogin)}
+              >
+                Add by login
+              </button>
+            </>
+          )}
         </div>
       )}
     </div>
