cfeb5c11efa4bf7ec789390d4f76b4f648d12403
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-08-09 22:24:50 +0200

    fix disconnect to send offline and fix some socket's service

--- server/src/chat/controller/socket/message_socket.py
+++ server/src/chat/controller/socket/message_socket.py
@@ -5,7 +5,7 @@ from flask_socketio import Namespace
 from src.chat.dto.message_dto import message_item
 from src.chat.service.message_service import save_new_message, valid_input_room, valid_input_message
 from src.chat.service.ws_service import (save_user_id_with_sid, delete_user_id_by_sid, get_user_id_by_sid,
-                                         user_join_into_project, user_leave_from_project, get_sid_by_user_id)
+                                         user_join_into_project, user_leave_from_project, get_sid_by_user_id_in_room)
 from src.chat.util.decorator import token_required
 
 
@@ -32,9 +32,8 @@ class WsMessageNamespace(Namespace):
         self.leave_room(request.sid, data.get('room'))
 
         # one user leave from project
-        user_id = get_user_id_by_sid()
-        user_leave_from_project(data.get('room'))
-        self.emit('offline', data=dict(user_id=user_id), room=data.get('room'), include_self=False)
+        data_leave = user_leave_from_project(data.get('room'))
+        self.emit('offline', data=dict(user_id=data_leave.get('user_id')), room=data_leave.get('room'), include_self=False)
 
     def on_send_message(self, data):
         data = valid_input_message(data)
@@ -46,10 +45,14 @@ class WsMessageNamespace(Namespace):
         if not message.receiver_id:
             self.emit('receive_message', data=message_dto, room=data.get('room'), include_self=False)
         else:
-            sid_receive = get_sid_by_user_id(message.receiver_id)
-            self.emit('receive_message', data=message_dto, room=sid_receive, include_self=False)
+            sid_receive = get_sid_by_user_id_in_room(message.receiver_id, data.get('room'))
+            if sid_receive:
+                self.emit('receive_message', data=message_dto, room=sid_receive, include_self=False)
 
         return message_dto
 
     def on_disconnect(self):
-        delete_user_id_by_sid()
+        data = delete_user_id_by_sid()
+        if data.get('room'):
+            user_leave_from_project(data.get('room'))
+            self.emit('offline', data=dict(user_id=data.get('user_id')), room=data.get('room'), include_self=False)
--- server/src/chat/service/ws_service.py
+++ server/src/chat/service/ws_service.py
@@ -1,37 +1,68 @@
-from typing import List
+import json
+from typing import List, Optional, Dict
 
 from flask import request
 
 from src.chat import redis
 
 
-def save_user_id_with_sid(user_id: int):
-    redis.set(f'sid:{request.sid}', user_id)
+def save_user_id_with_sid(user_id: int) -> None:
+    data = dict(user_id=user_id)
+    redis.set(f'sid:{request.sid}', json.dumps(data))
 
 
-def delete_user_id_by_sid():
-    redis.delete(f'sid:{request.sid}')
+def delete_user_id_by_sid() -> Dict:
+    data = redis.getdel(f'sid:{request.sid}')
+    return json.loads(data.decode("utf-8"))
 
 
 def get_user_id_by_sid() -> int:
-    return int(redis.get(f'sid:{request.sid}'))
+    data = redis.get(f'sid:{request.sid}')
+    data = json.loads(data.decode("utf-8"))
+    return data.get('user_id')
 
 
-def get_sid_by_user_id(user_id: int) -> str:
-    for key in redis.scan_iter('sid:*'):
-        if int(redis.get(key)) == user_id:
-            return key
+def get_sid_by_user_id_in_room(user_id: int, room: str) -> Optional[str]:
+    for user in get_all_user_in_room(room):
+        if user.get('user_id') == user_id:
+            return user.get('sid')
+    return None
 
 
-def user_join_into_project(room: str) -> List:
-    redis.sadd(room, get_user_id_by_sid())
+def get_all_user_in_room(room: str) -> List:
     return list(
         map(
-            lambda x: dict(user_id=int(x.decode("utf-8"))),
+            lambda x: json.loads(x.decode("utf-8")),
             list(redis.smembers(room)),
         )
     )
 
 
-def user_leave_from_project(room: str) -> None:
-    redis.srem(room, get_user_id_by_sid())
+def user_join_into_project(room: str) -> List:
+    data = json.dumps(
+        dict(
+            user_id=get_user_id_by_sid(),
+            sid=request.sid,
+            room=room
+        ),
+        sort_keys=True
+    )
+    redis.sadd(room, data)
+    redis.set(f'sid:{request.sid}', data)
+
+    return list(
+        map(
+            lambda x: dict(user_id=x.get('user_id')),
+            get_all_user_in_room(room),
+        )
+    )
+
+
+def user_leave_from_project(room: str) -> Dict:
+    data = dict(
+        user_id=get_user_id_by_sid(),
+        sid=request.sid,
+        room=room
+    )
+    redis.srem(room, json.dumps(data, sort_keys=True))
+    return data
