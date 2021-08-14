c2bf5b0647758a0cf8e6edd4219d989e8bbd2827
giacat-tran0198 <giacat.tran0198@gmail.com>
2021-08-14 17:42:51 +0200

    update push subscription

---
+++ server/src/chat/model/push_subscription.py
@@ -0,0 +1,17 @@
+"""Class definition for PushSubscription."""
+
+from src.chat import db
+
+
+class PushSubscription(db.Model):
+    """
+    Token Model for storing JWT tokens
+    """
+    __tablename__ = 'push_subscription'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, unique=True, nullable=False)
+    subscription_json = db.Column(db.Text, unique=True, nullable=False)
+
+    def __repr__(self):
+        return '<user_id: {}>'.format(self.user_id)
